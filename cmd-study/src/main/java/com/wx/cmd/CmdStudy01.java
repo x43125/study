d4887b481b144307f8705e8fb4c1b2e8dda5ba54
x43125 <wx43125@163.com>
2022-04-18 22:37:34 +0800

    update project structure

--- cmd-study/src/main/java/com/wx/cmd/CmdStudy01.java
+++ cmd-study/src/main/java/com/wx/cmd/CmdStudy01.java
@@ -12,10 +12,16 @@ public class CmdStudy01 {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to my System!");
 
+        /** TODO: add a init function 
+         * - init the return word
+         * - init the tips word
+         */
+
         try (Scanner scanner = new Scanner(System.in);) {
             boolean goNextLine = true;
             System.out.print("x> ");
             while (goNextLine) {
+                // TODO: add the function about keyboard shortcut key e.g.: ctrl
                 String nextLine = scanner.nextLine();
                 goNextLine = switchCase(nextLine);
                 System.out.print("x> ");
@@ -27,9 +33,11 @@ public class CmdStudy01 {
     }
 
     private static boolean switchCase(String cmd) {
+        String trimCmd = cmd.trim();
+
         if (cmd == null || "".equals(cmd))
             return true;
-        switch (cmd) {
+        switch (trimCmd) {
             case "hello":
                 System.out.println("hello world");
                 return true;
