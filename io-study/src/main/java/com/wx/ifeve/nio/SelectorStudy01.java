4fa0e6efb16fe7a48675e838b9e5d8c93212606a
x43125 <wx43125@163.com>
2022-04-23 23:50:15 +0800

    @Description:   - add java.nio study go on tomorrow @Author: pipe-dream-x @Date: 2022.04.22 @Bug No: 0

--- io-study/src/main/java/com/wx/ifeve/nio/SelectorStudy01.java
+++ io-study/src/main/java/com/wx/ifeve/nio/SelectorStudy01.java
@@ -1,11 +1,9 @@
 package com.wx.ifeve.nio;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -30,34 +28,44 @@ public class SelectorStudy01 {
                 if (channels > 0) {
                     Set<SelectionKey> selectionKeys = selector.selectedKeys();
                     Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                    ByteBuffer buffer = ByteBuffer.allocate(100);
                     while (iterator.hasNext()) {
                         SelectionKey key = iterator.next();
+                        iterator.remove();
+
                         if (key.isAcceptable()) {
+                            System.out.println("key.isAcceptable");
                             // 说明当前 channel是一个连接 channel，故应该将其注册到selector中
                             SocketChannel socketChannel = serverSocketChannel.accept();
+                            if (socketChannel == null) continue;
                             socketChannel.configureBlocking(false);
                             socketChannel.register(selector, SelectionKey.OP_READ);
                             System.out.println("客户端 " + socketChannel.getRemoteAddress() + " 连接成功");
                         } else if (key.isReadable()) {
-                            SocketChannel socketChannel = (SocketChannel) key.channel();
-                            socketChannel.configureBlocking(false);
-                            ByteBuffer buffer = ByteBuffer.allocate(1024);
-                            socketChannel.read(buffer);
-                            System.out.println(new String(buffer.array()));
+                            System.out.println("key.isReadable");
+                            SocketChannel socketChannel = null;
+
+                            try {
+                                socketChannel = (SocketChannel) key.channel();
+                                socketChannel.configureBlocking(false);
+                                socketChannel.read(buffer);
+                                String msg = new String(buffer.array());
+                                System.out.println(msg.trim());
+                                buffer.clear();
+                            } catch (IOException e) {
+                                System.out.println("客户端下线");
+                                socketChannel.close();
+                            }
                         } else if (key.isConnectable()) {
-
+                            System.out.println("key.isConnectable");
                         } else if (key.isWritable()) {
-
+                            System.out.println("key.isWritable");
                         }
-                        iterator.remove();
                     }
                 }
             }
-
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
