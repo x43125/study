4f2de5c0c711b8ad23dcc827a4685f025e071928
x43125 <wx43125@163.com>
2022-04-27 09:14:37 +0800

    add netty study

---
+++ io-study/src/main/java/com/wx/netty/aobing/DemoSocketClientHandler.java
@@ -0,0 +1,36 @@
+package com.wx.netty.aobing;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description:
+ * @Author: wx
+ * @Date: 2022/04/25 15:54
+ */
+public class DemoSocketClientHandler
+        extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        System.out.println(msg);
+        ctx.channel().writeAndFlush("from client: " + System.currentTimeMillis());
+        TimeUnit.MILLISECONDS.sleep(5000);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx)
+            throws Exception {
+        ctx.channel().writeAndFlush("from client：begin talking");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
