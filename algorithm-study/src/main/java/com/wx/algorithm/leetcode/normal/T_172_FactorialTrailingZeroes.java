c0f0efa13a9a5c53c9ccb0e9a133fcc8c8d653e2
x43125 <wx43125@163.com>
2022-08-29 23:52:15 +0800

    @Description:   - add t_172 @Author: pipe-dream-x @Bug No: 0

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/T_172_FactorialTrailingZeroes.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/T_172_FactorialTrailingZeroes.java
@@ -6,23 +6,43 @@ package com.wx.algorithm.leetcode.normal;
  * @Date: 22/08/28
  */
 public class T_172_FactorialTrailingZeroes {
+
     public static void main(String[] args) {
-        int i = 225;
+        int i = 625;
         T_172_FactorialTrailingZeroes zeroes = new T_172_FactorialTrailingZeroes();
         int res = zeroes.trailingZeroes(i);
+        int res2 = zeroes.trailingZeroes_2(i);
         System.out.println("res = " + res);
+        System.out.println("res2 = " + res2);
     }
 
     public int trailingZeroes(int n) {
         int res = 0;
 
         for (int i = 5; i <= n; i += 5) {
-            String sqrt = String.valueOf(Math.sqrt(i));
-            if (sqrt.indexOf(".") == sqrt.length() - 2) {
-                res++;
-            }
-            res++;
+            res += getNo(i);
         }
         return res;
     }
+
+
+    int getNo(int num) {
+        int count = 0;
+        while (num % 5 == 0) {
+            count++;
+            num /= 5;
+        }
+        return count;
+    }
+
+    public int trailingZeroes_2(int n) {
+        int res = 0;
+        while (n > 0) {
+            n /= 5;
+            res += n;
+        }
+
+        return res;
+    }
+
 }
--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/T_793_KZero.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/T_793_KZero.java
@@ -1,14 +1,70 @@
 package com.wx.algorithm.leetcode.normal;
 
 
-
 public class T_793_KZero {
 
     public int preimageSizeFZF(int k) {
+        int ans = 0;
+        int left = -1;
+        int right;
+
+        while (true) {
+            int zeroNoNo = trailingZeroes(ans);
+            if (zeroNoNo == k) {
+                left = ans;
+            }
+            if (zeroNoNo > k) {
+                right = ans;
+                break;
+            }
+            ans += 5;
+        }
+
+        if (left == -1) {
+            return 0;
+        } else {
+            return right - left;
+        }
+    }
+
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n > 0) {
+            n /= 5;
+            res += n;
+        }
+
+        return res;
+    }
+
+    public int preimageSizeFZF_2(int k) {
+        long l = k - 1, r = k * 10L + 1;
+        while (l + 1 < r) {
+            long m = l + r >> 1, t = f(m);
+            if (t == k) {
+                return 5;
+            } else if (t < k) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
         return 0;
     }
 
+    long f(long n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n / 5 + f(n / 5);
+    }
+
     public static void main(String[] args) {
-        
+        int k = 98918711;
+        T_793_KZero kZero = new T_793_KZero();
+        int res = kZero.preimageSizeFZF(k);
+        System.out.println("res = " + res);
+//        int res2 = kZero.preimageSizeFZF_2(k);
+//        System.out.println("res2 = " + res2);
     }
 }
