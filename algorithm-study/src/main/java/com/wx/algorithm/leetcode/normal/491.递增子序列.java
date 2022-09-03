c34b55cb6df505c7171137ce17f2acfa55afa99f
wangxiang <wangxiang@flashhold.com>
2022-09-03 16:23:17 +0800

    add t_646 greedy algorithm

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/491.递增子序列.java
@@ -0,0 +1,18 @@
+package com.wx.algorithm.leetcode.normal;
+
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=491 lang=java
+ *
+ * [491] 递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/646.最长数对链.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/646.最长数对链.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @Author: wangxiang wangxiang@flashhold.com
  * @Date: 2022-09-03 14:01:43
  * @LastEditors: wangxiang wangxiang@flashhold.com
- * @LastEditTime: 2022-09-03 14:47:49
+ * @LastEditTime: 2022-09-03 16:22:00
  * @FilePath: \algorithm-study\src\main\java\com\wx\algorithm\leetcode\normal\646.最长数对链.java
  * @Description: 
  * 定义 \textit{dp}[i]dp[i] 为以 \textit{pairs}[i]pairs[i] 为结尾的最长数对链的长度。
@@ -26,10 +26,11 @@ class Solution {
 
     /**
      * dp方式
+     * 
      * @param pairs
      * @return
      */
-    public int findLongestChain(int[][] pairs) {
+    public int findLongestChain_2(int[][] pairs) {
         int n = pairs.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
@@ -45,6 +46,29 @@ class Solution {
         }
 
         return dp[n - 1];
+    }
+
+    /*
+     * 要挑选最长数对链的第一个数对时，
+     * 最优的选择是挑选第二个数字最小的，
+     * 这样能给挑选后续的数对留下更多的空间。
+     * 挑完第一个数对后，要挑第二个数对时，
+     * 也是按照相同的思路，是在剩下的数对中，
+     * 第一个数字满足题意的条件下，挑选第二个数字最小的。
+     * 按照这样的思路，可以先将输入按照第二个数字排序，
+     * 然后不停地判断第一个数字是否能满足大于前一个数对的第二个数字即可。
+     */
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int count = 0;
+        int last = Integer.MIN_VALUE;
+        for (int[] pair : pairs) {
+            if (last < pair[0]) {
+                count++;
+                last = pair[1];
+            }
+        }
+        return count;
     }
 }
 // @lc code=end
