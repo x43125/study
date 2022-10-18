d72414c37576d2388faa7c199991b98973e75649
wangxiang <wangxiang@flashhold.com>
2022-10-18 10:14:01 +0800

    update 850

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/normal/850.矩形面积-ii.java
@@ -0,0 +1,24 @@
+/*
+ * @Author: wangxiang wangxiang@flashhold.com
+ * @Date: 2022-09-16 22:07:47
+ * @LastEditors: wangxiang wangxiang@flashhold.com
+ * @LastEditTime: 2022-09-16 22:11:09
+ * @FilePath: \algorithm-study\src\main\java\com\wx\algorithm\leetcode\normal\850.矩形面积-ii.java
+ * @Description: 
+ */
+package com.wx.algorithm.leetcode.normal;
+
+/*
+ * @lc app=leetcode.cn id=850 lang=java
+ *
+ * [850] 矩形面积 II
+ */
+
+// @lc code=start
+class Solution {
+    public int rectangleArea(int[][] rectangles) {
+        
+    }
+}
+// @lc code=end
+
