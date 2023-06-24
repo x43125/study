82411f4c9d57b09ad74f21a1b45dfe7f7e6f9a0e
x43125 <wx43125@163.com>
2023-06-24 22:22:13 +0800

    T73 暴力法

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T73SetZeroes.java
@@ -0,0 +1,42 @@
+package com.wx.algorithm.leetcode.hot100;
+
+/**
+ * @author wangxiang
+ * @date 2023/6/24 22:08
+ * @description
+ */
+public class T73SetZeroes {
+    public static void main(String[] args) {
+//        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroes(matrix);
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = cols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
