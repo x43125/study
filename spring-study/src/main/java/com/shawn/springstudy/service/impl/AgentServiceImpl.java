b9ebc2c3d313b82e0bd989e160dddaba65254594
x43125 <wx43125@163.com>
2023-06-25 16:53:24 +0800

    add spring-ioc: @Resource; @Inject

--- spring-study/src/main/java/com/shawn/springstudy/service/impl/AgentServiceImpl.java
+++ spring-study/src/main/java/com/shawn/springstudy/service/impl/AgentServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * @author wangxiang
  * @date 2023/6/25 16:29
@@ -14,9 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class AgentServiceImpl implements AgentService {
 
+//    @Qualifier("clientDaoImpl")
+    @Qualifier("agentDaoImpl")
     @Autowired
-//    @Qualifier("agentDaoImpl")
-    @Qualifier("clientDaoImpl")
+//    @Resource(name = "agentDaoImpl")
+//    @Resource(name = "clientDaoImpl")
+
+    // @Inject
+    // @Named("agentDaoImpl")
     private AgentDao agentDao;
 
     @Override
