f6e93fed53f7d3f4cb9a307e6cb86a224b5fe137
x43125 <wx43125@163.com>
2023-06-27 14:03:14 +0800

    T142 其实是找规律题

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T141HasCycle.java
@@ -0,0 +1,27 @@
+package com.wx.algorithm.leetcode.hot100;
+
+import com.wx.algorithm.base.model.ListNode;
+
+/**
+ * @author wangxiang
+ * @date 2023/6/27 13:51
+ * @description
+ */
+public class T141HasCycle {
+    public static void main(String[] args) {
+
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+        while (slow != fast && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast != null && fast.next != null;
+    }
+}
