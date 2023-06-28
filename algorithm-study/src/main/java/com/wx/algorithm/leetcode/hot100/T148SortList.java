413c5e799584f7b96da952fbcb1671ea1c300f3f
x43125 <wx43125@163.com>
2023-06-28 08:09:01 +0800

    modified sortlist

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T148SortList.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T148SortList.java
@@ -1,7 +1,14 @@
-package com.wx.algorithm.leetcode.hot100;/**
-* @author wangxiang
-* @date 2023/6/28 07:53
-* @description 
-*/
+package com.wx.algorithm.leetcode.hot100;
+
+import com.wx.algorithm.base.model.ListNode;
+
+/**
+ * @author wangxiang
+ * @date 2023/6/28 07:53
+ * @description
+ */
 public class T148SortList {
+    public ListNode sortList(ListNode head) {
+        
+    }
 }
