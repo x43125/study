6a74c36395e23331beb35d6fa6eafd4a8c84d96d
x43125 <wx43125@163.com>
2023-06-28 23:39:21 +0800

    add spring-aop-study

--- spring/spring-aop-study-01/src/main/java/com/shawn/springaopstudy01/SpringAopStudy01Application.java
+++ spring/spring-aop-study-01/src/main/java/com/shawn/springaopstudy01/SpringAopStudy01Application.java
@@ -1,5 +1,8 @@
 package com.shawn.springaopstudy01;
 
+import com.shawn.springaopstudy01.service.MyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,9 +10,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SpringAopStudy01Application {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringAopStudy01Application.class, args);
+    private static final Logger log = LoggerFactory.getLogger(SpringAopStudy01Application.class);
 
+    public static void main(String[] args) {
+//        ConfigurableApplicationContext context = SpringApplication.run(SpringAopStudy01Application.class, args);
+//        MyService service = context.getBean(MyService.class);
+//        log.info("service class: {}", service.getClass());
+//        service.foo();
+//        context.close();
+
+        new MyService().foo();
     }
 
 }
--- spring/spring-aop-study-01/src/main/java/com/shawn/springaopstudy01/aop/MyAspect.java
+++ spring/spring-aop-study-01/src/main/java/com/shawn/springaopstudy01/aop/MyAspect.java
@@ -1,8 +1,10 @@
 package com.shawn.springaopstudy01.aop;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * @author wangxiang
@@ -10,10 +12,12 @@ import org.slf4j.LoggerFactory;
  * @description
  */
 @Aspect
+//@Component
 public class MyAspect {
     private static final Logger log = LoggerFactory.getLogger(MyAspect.class);
 
+    @Before("execution(* com.shawn.springaopstudy01.service.MyService.foo())")
     public void before() {
-        
+        log.info("before()");
     }
 }
