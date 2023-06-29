9a856747cd5d8fd4ee76ace78aff5474ac71a6af
x43125 <wx43125@163.com>
2023-06-29 13:39:16 +0800

    归并排序：递归

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T148SortList.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T148SortList.java
@@ -8,7 +8,63 @@ import com.wx.algorithm.base.model.ListNode;
  * @description
  */
 public class T148SortList {
+
+    /**
+     * 自顶向下归并
+     *
+     * @param head
+     * @return
+     */
     public ListNode sortList(ListNode head) {
-        
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode temp = preHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val < temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (temp1 != null) {
+            temp.next = temp1;
+        }
+        if (temp2 != null) {
+            temp.next = temp2;
+        }
+
+        return preHead.next;
     }
 }
