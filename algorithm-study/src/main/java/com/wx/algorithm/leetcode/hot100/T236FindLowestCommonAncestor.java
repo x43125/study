d229556530158fdf681de06f15fd3103d516c6cc
x43125 <wx43125@163.com>
2023-07-02 10:42:08 +0800

    暴力法

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T236FindLowestCommonAncestor.java
@@ -0,0 +1,56 @@
+package com.wx.algorithm.leetcode.hot100;
+
+import com.wx.algorithm.base.model.TreeNode;
+
+/**
+ * @author wangxiang
+ * @date 2023/7/2 10:05
+ * @description
+ */
+public class T236FindLowestCommonAncestor {
+    /**
+     * 两节点如果都在某节点的一侧的话，则说明不是
+     * 两节点如果刚好分布在某节点两侧的话，则是
+     * 两节点其中一节点在另一节点下，也是
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p) {
+            if (traverse(root, q)) {
+                return p;
+            }
+        } else if (root == q) {
+            if (traverse(root, p)) {
+                return q;
+            }
+        } else {
+            if ((traverse(root.left, p) && traverse(root.right, q))
+                    || (traverse(root.left, q) && traverse(root.right, p))) {
+                return root;
+            }
+        }
+
+        TreeNode node = lowestCommonAncestor(root.left, p, q);
+        return node == null ? lowestCommonAncestor(root.right, p, q) : node;
+    }
+
+    private boolean traverse(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return false;
+        }
+        if (root == node) {
+            return true;
+        }
+
+        return traverse(root.left, node) || traverse(root.right, node);
+    }
+
+
+}
