3ca486f21a8057af662297b6a0967cf3b30c8eeb
x43125 <wx43125@163.com>
2023-07-03 16:01:11 +0800

    拓扑

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T207CoursesCanFinish.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T207CoursesCanFinish.java
@@ -1,7 +1,9 @@
 package com.wx.algorithm.leetcode.hot100;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author wangxiang
@@ -58,5 +60,60 @@ public class T207CoursesCanFinish {
         T207CoursesCanFinish coursesCanFinish = new T207CoursesCanFinish();
         boolean canFinish = coursesCanFinish.canFinish(numCourses, prerequisites);
         System.out.println("canFinish = " + canFinish);
+    }
+
+
+    int[] inDeg;
+
+    /**
+     * 难点在于如何维护边、节点、入度、出度的信息
+     * 像本题，使用了一个集合 edges 来表示某个节点指向的节点；使用了一个数组 inDeg 来表示某个节点的入度
+     * 通过维护 inDeg 来查看是否能够找到目标值
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinishIn(int numCourses, int[][] prerequisites) {
+        // 初始化
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        // 初始化 所有节点的入度
+        inDeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            // 节点info[0]的前置节点info[1]
+            // 记录info[1]节点的出边
+            edges.get(info[1]).add(info[0]);
+            // 节点info[0]的入度+1
+            ++inDeg[info[0]];
+        }
+
+        // 记录所有入度为0的节点
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            // 扫描的个数
+            ++visited;
+            int u = queue.poll();
+            // 当前节点 u 的所有出边 即u指向的下一个节点
+            for (Integer v : edges.get(u)) {
+                // 下一个节点的入度-1
+                --inDeg[v];
+                // 如果该点的入度为0，则维护进队列里
+                if (inDeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
     }
 }
