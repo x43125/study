8b3e672cb6994365b55ed015c74fc9fd7606ab9e
x43125 <wx43125@163.com>
2023-07-04 16:12:46 +0800

    查找单词，自己做出来了，很棒，嘿嘿

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T79SearchWord.java
@@ -0,0 +1,61 @@
+package com.wx.algorithm.leetcode.hot100;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author wangxiang
+ * @date 2023/7/4 14:49
+ * @description
+ */
+public class T79SearchWord {
+    boolean ans;
+
+    public boolean exist(char[][] board, String word) {
+        ans = false;
+        int m = board.length, n = board[0].length;
+
+        boolean[][] used = new boolean[m][n];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                traverse(board, word, sb, i, j, used);
+                if (ans) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void traverse(char[][] board, String word, StringBuilder sb, int i, int j, boolean[][] used) {
+        if (ans || word.contentEquals(sb)) {
+            ans = true;
+            return;
+        }
+        if (i < 0 || i >= board.length
+                || j < 0 || j >= board[0].length
+                || used[i][j]
+                || board[i][j] != word.charAt(sb.length())) {
+            return;
+        }
+
+        used[i][j] = true;
+        sb.append(board[i][j]);
+        traverse(board, word, sb, i - 1, j, used);
+        traverse(board, word, sb, i + 1, j, used);
+        traverse(board, word, sb, i, j - 1, used);
+        traverse(board, word, sb, i, j + 1, used);
+        sb.deleteCharAt(sb.length() - 1);
+        used[i][j] = false;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{'E', 'A', 'B', 'C'}, {'S', 'S', 'F', 'C'}, {'E', 'A', 'D', 'E'}};
+        String word = "ABCCED";
+        T79SearchWord searchWord = new T79SearchWord();
+        boolean exist = searchWord.exist(board, word);
+        System.out.println("exist = " + exist);
+    }
+}
