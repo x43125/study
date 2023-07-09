8b7ca4104e2354ce2663813da552fab56c98a890
x43125 <wx43125@163.com>
2023-07-09 17:19:53 +0800

    自定义线程池：Thread pool; reject policy

--- base/src/main/java/com/wx/base/juc/advance/pool/ThreadPoolCustom.java
+++ base/src/main/java/com/wx/base/juc/advance/pool/ThreadPoolCustom.java
@@ -2,6 +2,7 @@ package com.wx.base.juc.advance.pool;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -12,7 +13,98 @@ import java.util.concurrent.locks.ReentrantLock;
  * @description
  */
 public class ThreadPoolCustom {
+    public static void main(String[] args) {
+        ThreadPool pool = new ThreadPool(1, 1000, TimeUnit.MILLISECONDS, 1, ((queue, task) -> {
+            // 1.死等
+//            queue.put(task);
+            // 2.带超时等待
+            queue.offer(task, 500, TimeUnit.MILLISECONDS);
+        }));
 
+        for (int i = 0; i < 3; i++) {
+            int j = i;
+            pool.execute(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(j);
+            });
+        }
+
+    }
+}
+
+@FunctionalInterface
+interface RejectPolicy<T> {
+    void reject(BlockingQueue<T> queue, T task);
+}
+
+class ThreadPool {
+    /**
+     * 任务队列：任务具体要执行的内容集合
+     */
+    private BlockingQueue<Runnable> taskQueue;
+    /**
+     * 线程集合：Thread集合，用于执行上面的任务
+     */
+    private HashSet<Worker> workers = new HashSet<>();
+    private int coreSize;
+    private long timeout;
+    private TimeUnit timeUnit;
+
+    private RejectPolicy<Runnable> rejectPolicy;
+
+    public void execute(Runnable task) {
+        synchronized (workers) {
+            if (workers.size() < coreSize) {
+                Worker worker = new Worker(task);
+                System.out.println("新增 worker: " + worker + ", " + task);
+                workers.add(worker);
+                worker.start();
+            } else {
+//                taskQueue.put(task);
+                taskQueue.tryPut(rejectPolicy, task);
+            }
+        }
+    }
+
+    public ThreadPool(int coreSize, long timeout, TimeUnit timeUnit, int queueCapacity, RejectPolicy<Runnable> rejectPolicy) {
+        this.coreSize = coreSize;
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
+        this.taskQueue = new BlockingQueue<>(queueCapacity);
+        this.rejectPolicy = rejectPolicy;
+    }
+
+    class Worker extends Thread {
+        private Runnable task;
+
+        public Worker(Runnable task) {
+            this.task = task;
+        }
+
+        @Override
+        public void run() {
+            // 执行任务
+            // 1）当task不为空，执行任务
+            // 2）当task执行完毕，再接着从任务队列获取任务并执行
+            while (task != null || (task = taskQueue.take()) != null) {
+                try {
+                    task.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    task = null;
+                }
+            }
+
+            synchronized (workers) {
+                workers.remove(this);
+            }
+        }
+    }
 }
 
 class BlockingQueue<T> {
@@ -36,6 +128,10 @@ class BlockingQueue<T> {
      * 容量:最大容量
      */
     private int capacity;
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
 
     public T poll(long timeout, TimeUnit timeUnit) {
         lock.lock();
@@ -69,10 +165,10 @@ class BlockingQueue<T> {
     public T take() {
         lock.lock();
         try {
-            // 当队列为空的时候，等待;当不为空的时候，则返回头节点任务
+            // 当队列为空的时候，等待; 当不为空的时候，则返回头节点任务
             while (queue.isEmpty()) {
                 // 等待队列插值的时候唤醒
-                emptyWaitSet.wait();
+                emptyWaitSet.await();
             }
             T headTask = queue.removeFirst();
             // 当取出了一个任务后，队列则非满状态，此时可以唤醒 fullWaitSet 等待
@@ -85,13 +181,48 @@ class BlockingQueue<T> {
         }
     }
 
+    public boolean offer(T task, long timeout, TimeUnit timeUnit) {
+        lock.lock();
+        try {
+            long nanos = timeUnit.toNanos(timeout);
+            while (queue.size() == capacity) {
+                System.out.println("等待加入任务队列: " + task);
+                if (nanos <= 0) {
+                    return false;
+                }
+                nanos = fullWaitSet.awaitNanos(nanos);
+            }
+            // 当添加一个任务进队列后，队列则非空状态，此时可以唤醒 emptyWaitSet 等待
+            System.out.println("加入任务队列: " + task);
+            queue.addLast(task);
+            emptyWaitSet.signal();
+            return true;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public int size() {
+        lock.lock();
+        try {
+            return queue.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
     public void put(T task) {
         lock.lock();
         try {
             while (queue.size() == capacity) {
-                fullWaitSet.wait();
+                System.out.println("等待加入任务队列: " + task);
+                fullWaitSet.await();
             }
             // 当添加一个任务进队列后，队列则非空状态，此时可以唤醒 emptyWaitSet 等待
+            System.out.println("加入任务队列: " + task);
             queue.addLast(task);
             emptyWaitSet.signal();
         } catch (InterruptedException e) {
@@ -101,10 +232,16 @@ class BlockingQueue<T> {
         }
     }
 
-    public int size() {
+    public void tryPut(RejectPolicy<T> rejectPolicy, T task) {
         lock.lock();
         try {
-            return queue.size();
+            if (queue.size() == capacity) {
+                rejectPolicy.reject(this, task);
+            } else {
+                System.out.println("加入任务队列: " + task);
+                queue.addLast(task);
+                emptyWaitSet.signal();
+            }
         } finally {
             lock.unlock();
         }
