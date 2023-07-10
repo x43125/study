9021ce1a4dac488305f3e2ff90a1ce25bb38667f
x43125 <wx43125@163.com>
2023-07-10 09:52:25 +0800

    partition

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T131Partition.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hot100/T131Partition.java
@@ -13,43 +13,85 @@ import java.util.List;
  * @description
  */
 public class T131Partition {
-    boolean[][] flag;
-    List<List<String>> ret = new ArrayList<>();
-    List<String> ans = new ArrayList<>();
+//    boolean[][] flag;
+//    List<List<String>> ret = new ArrayList<>();
+//    List<String> ans = new ArrayList<>();
+//    int n;
+//
+//    public List<List<String>> partition(String s) {
+//        n = s.length();
+//        flag = new boolean[n][n];
+//        for (int i = 0; i < n; ++i) {
+//            Arrays.fill(flag[i], true);
+//        }
+//
+//        for (int i = n - 1; i >= 0; --i) {
+//            for (int j = i + 1; j < n; ++j) {
+//                // 动态规划，判断是否是回文串：即当前最外侧两值是否相等 && 内侧值是否是回文串
+//                // flag[i][j] 表示，i~j 的字符串是否是回文串；所以i>j的位置没用，只用右上角的内容
+//                // 比如 str="aab"; flag[0][2]: 表示 aab 是否是回文串
+//                // flag[1][2]: 表示 ab 是否是回文串
+//                flag[i][j] = (s.charAt(i) == s.charAt(j)) && flag[i + 1][j - 1];
+//            }
+//        }
+//
+//        dfs(s, 0);
+//        return ret;
+//    }
+//
+//    public void dfs(String s, int i) {
+//        if (i == n) {
+//            ret.add(new ArrayList<>(ans));
+//            return;
+//        }
+//
+//        for (int j = i; j < n; ++j) {
+//            if (flag[i][j]) {
+//                ans.add(s.substring(i, j + 1));
+//                dfs(s, j + 1);
+//                ans.remove(ans.size() - 1);
+//            }
+//        }
+//    }
+
+    List<List<String>> ans;
+    List<String> nowAns;
+    boolean[][] palindromic;
     int n;
 
     public List<List<String>> partition(String s) {
+        ans = new ArrayList<>();
+        nowAns = new ArrayList<>();
         n = s.length();
-        flag = new boolean[n][n];
-        for (int i = 0; i < n; ++i) {
-            Arrays.fill(flag[i], true);
+        // 先把所有回文串通过动态规划搞出来
+        palindromic = new boolean[n][n];
+        // 先全部赋true
+        for (int i=0; i<n; i++) {
+            Arrays.fill(palindromic[i], true);
         }
 
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                // 动态规划，判断是否是回文串：即当前最外侧两值是否相等 && 内侧值是否是回文串
-                // flag[i][j] 表示，i~j 的字符串是否是回文串；所以i>j的位置没用，只用右上角的内容
-                // 比如 str="aab"; flag[0][2]: 表示 aab 是否是回文串
-                // flag[1][2]: 表示 ab 是否是回文串
-                flag[i][j] = (s.charAt(i) == s.charAt(j)) && flag[i + 1][j - 1];
+        for (int i=n-1; i>=0; i--) {
+            for (int j=i+1; j<n; j++) {
+                palindromic[i][j] = (s.charAt(i) == s.charAt(j)) && palindromic[i+1][j-1];
             }
         }
 
         dfs(s, 0);
-        return ret;
+        return ans;
     }
 
-    public void dfs(String s, int i) {
+    private void dfs(String s, int i) {
         if (i == n) {
-            ret.add(new ArrayList<>(ans));
+            ans.add(new ArrayList<>(nowAns));
             return;
         }
 
-        for (int j = i; j < n; ++j) {
-            if (flag[i][j]) {
-                ans.add(s.substring(i, j + 1));
-                dfs(s, j + 1);
-                ans.remove(ans.size() - 1);
+        for (int j=i; j<n; j++) {
+            // 判断从i到末尾的每一个字符串是否是回文串，如果是则加入
+            if (palindromic[i][j]) {
+                nowAns.add(s.substring(i, j+1));
+                dfs(s, j+1);
+                nowAns.remove(nowAns.size()-1);
             }
         }
     }
