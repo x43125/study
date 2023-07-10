b2c0a7d9f9ffbe79c37728fb98b96ac51234e3a0
x43125 <wx43125@163.com>
2023-07-10 23:43:19 +0800

    雪花算法的Java实现

---
+++ base/src/main/java/com/wx/base/CommonStudy01.java
@@ -0,0 +1,15 @@
+package com.wx.base;
+
+import java.util.UUID;
+
+/**
+ * @author Shawn
+ * @date 2023/7/10 23:36
+ * @description
+ */
+public class CommonStudy01 {
+    public static void main(String[] args) {
+        // 75f21fdf-b00d-4437-8e00-d5c60f8f8f3d
+        System.out.println("UUID.randomUUID() = " + UUID.randomUUID());
+    }
+}
