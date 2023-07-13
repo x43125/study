f603c4159ed6a4ee43adc449e03d16ca02489e07
x43125 <wx43125@163.com>
2023-07-13 16:53:58 +0800

    update

--- base/src/main/java/com/wx/base/juc/base/GuidedStudy01.java
+++ base/src/main/java/com/wx/base/juc/base/GuidedStudy01.java
@@ -1,5 +1,6 @@
 package com.wx.base.juc.base;
 
+import com.wx.base.juc.advance.utils.JUCUtils;
 import com.wx.base.juc.base.model.GuidedObject;
 import com.wx.base.juc.base.util.Download;
 
@@ -16,13 +17,22 @@ public class GuidedStudy01 {
     public static void main(String[] args) throws InterruptedException {
         GuidedObject guidedObject = new GuidedObject();
         AtomicReference<List<String>> lines = new AtomicReference<>();
+        // 取得下载的内容，如果还未下载则阻塞等待
         Thread t1 = new Thread(() -> {
-            lines.set((List<String>) guidedObject.get());
+            JUCUtils.sysout("获取下载内容，如果尚未下载，则阻塞此线程，先去下载");
+            List<String> downloadContent = (List<String>) guidedObject.get();
+            JUCUtils.sysout("获取到下载内容，装载");
+            lines.set(downloadContent);
+            JUCUtils.sleeper(10000000);
         }, "t1");
 
         new Thread(() -> {
             try {
+                // 执行下载操作
+                JUCUtils.sysout("下载...");
                 List<String> content = Download.download();
+                // 将下载结果保存起来
+                JUCUtils.sysout("保存下载的内容");
                 guidedObject.complete(content);
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -30,9 +40,8 @@ public class GuidedStudy01 {
         }, "t2").start();
 
         t1.start();
-        t1.join();
+        t1.join(5000);
         System.out.println(lines.get().size());
         lines.get().forEach(System.out::println);
-
     }
 }
