c1a60023bc73a667501c6e089a0c51786a0d52a9
x43125 <wx43125@163.com>
2023-07-27 00:40:03 +0800

    add spring cloud alibaba study project

---
+++ spring-cloud-study/springcloud-alibaba-parent/springcloud-alibaba_nacos-client01/src/main/java/com/shawn/NacosClient01Application.java
@@ -0,0 +1,11 @@
+package com.shawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClient01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClient01Application.class, args);
+    }
+}
