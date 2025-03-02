4be2d37f29c5c0402ea43d184a74dcf465fea959
x43125 <wx43125@163.com>
2025-03-02 22:53:03 +0800

    quick sort

--- algorithm-study/src/main/java/com/wx/algorithm/algorithm4/sort/QuickSort.java
+++ algorithm-study/src/main/java/com/wx/algorithm/algorithm4/sort/QuickSort.java
@@ -4,7 +4,6 @@ package com.wx.algorithm.algorithm4.sort;
  * 快排
  */
 public class QuickSort extends SortBase {
-
     @Override
     public void sort(Comparable[] a) {
         sort(a, 0, a.length - 1);
@@ -14,28 +13,27 @@ public class QuickSort extends SortBase {
         if (high <= low) {
             return;
         }
-
         int j = partition(a, low, high);
-        sort(a, low, j - 1);
-        sort(a, j + 1, high);
+        sort(a, low, j-1);
+        sort(a, j+1, high);
     }
 
+    /**
+     * 既分组又排序
+     * @param a
+     * @param low
+     * @param high
+     * @return
+     */
     private int partition(Comparable[] a, int low, int high) {
-        // 因为下面用的是 ++i和--j，所以i起始从low开始，因为low本身就不需要比较，而j从high+1开始
         int i = low, j = high + 1;
         Comparable v = a[low];
-
         while (true) {
-            while (i<high && less(a[++i], v)) {
-            }
-            while (j>low && less(v, a[--j])) {
-            }
-            if (i >= j) {
-                break;
-            }
+            while (less(a[++i], v) && i < high);
+            while (less(v, a[--j]) && j > low);
+            if (i >= j) break;
             exch(a, i, j);
         }
-
         exch(a, low, j);
         return j;
     }
