b936b5a6f1b90b953236b398e397da0397b8ef2d
x43125 <wx43125@163.com>
2025-03-09 19:45:47 +0800

    maxPQ

--- algorithm-study/src/main/java/com/wx/algorithm/algorithm4/sort/MaxPQ.java
+++ algorithm-study/src/main/java/com/wx/algorithm/algorithm4/sort/MaxPQ.java
@@ -76,4 +76,16 @@ public class MaxPQ<Key extends Comparable<Key>> {
             k = j;
         };
     }
+
+    // public void sort(Comparable[] a) {
+    //     int n = a.length;
+    //     for (int k = n/2; k>=1; k--) {
+    //         sink(a, k, n);
+    //     }
+
+    //     while (n > 1) {
+    //         exch(a, 1, n--);
+    //         sink(a, 1, n);
+    //     }
+    // }
 }
