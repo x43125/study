1e4c5a9c67d86f0ad33df2ea54677b0b1607d930
x43125 <wx43125@163.com>
2025-03-11 23:45:57 +0800

    max slide window

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/T_239_MaxSlideWindow.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/T_239_MaxSlideWindow.java
@@ -1,18 +1,25 @@
 package com.wx.algorithm.leetcode.hop2025;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class T_239_MaxSlideWindow {
     public static void main(String[] args) {
-        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int[] nums = { 1, 3, -1, -3, -2, 3, 6, 7 };
+        // int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
         int k = 3;
         T_239_MaxSlideWindow maxSlideWindow = new T_239_MaxSlideWindow();
-        int[] res = maxSlideWindow.maxSlidingWindow(nums, k);
+        int[] res = maxSlideWindow.maxSlidingWindow2(nums, k);
         for (int max : res) {
-            System.out.println(max);
+            System.out.print(max + " ");
         }
+        System.out.println();
     }
 
     /**
      * 暴力法
+     * 
      * @param nums
      * @param k
      * @return
@@ -21,11 +28,35 @@ public class T_239_MaxSlideWindow {
         int[] res = new int[nums.length - k + 1];
         for (int i = 0; i < nums.length - k + 1; i++) {
             int max = Integer.MIN_VALUE;
-            for (int j = i; j < i+k; j++) {
+            for (int j = i; j < i + k; j++) {
                 max = Math.max(max, nums[j]);
             }
             res[i] = max;
         }
+        return res;
+    }
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        Queue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] != o2[1] ? o2[0] - o1[0] : o2[1] - o1[1];
+            }
+        });
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < k; i++) {
+            q.offer(new int[]{nums[i], i});
+        }
+        res[0] = q.peek()[0];
+
+        for (int i = k; i < nums.length; i++) {
+            q.offer(new int[]{nums[i], i});
+            while (q.peek()[1] < i-k+1) {
+                q.poll();
+            }
+            res[i-k+1] = q.peek()[0];
+        }
+
         return res;
     }
 }
