5abcc18ae4bdf4428ed9b57d76b32198de5f0867
x43125 <wx43125@163.com>
2025-03-15 15:49:08 +0800

    three sum 更好理解的方式

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_15_ThreeSum.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_15_ThreeSum.java
@@ -6,31 +6,41 @@ import java.util.List;
 
 public class T_15_ThreeSum {
 
+    // -4 -1 -1 0 1 2
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        // 排序
         Arrays.sort(nums);
         int n = nums.length;
-        List<List<Integer>> result = new ArrayList<>();
+        // 遍历
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // 如果
                 continue;
-            int target = -nums[i]; // 找-nums[i]
-            int k = n - 1;
-            for (int j = i + 1; j < n - 1; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
-
-                while (j < k && nums[j] + nums[k] > target) {
+            }
+            // 转变为找剩下的两数之和
+            int target = -nums[i];
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int temp = nums[j] + nums[k];
+                if (temp == target) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    // 看下一组是否符合
+                    j++;
                     k--;
-                }
-
-                if (j == k)
-                    break;
-                if (nums[j] + nums[k] == target) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    result.add(list);
+                    // 跳过重复的情况
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (temp < target) {
+                    // 左指针右移
+                    j++;
+                } else {
+                    // 右指针左移
+                    k--;
                 }
             }
         }
@@ -39,7 +49,8 @@ public class T_15_ThreeSum {
     }
 
     public static void main(String[] args) {
-        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        // int[] nums = { -1, 0, 1, 2, -1, -4 };
+        int[] nums = { 0, 0, 0, 0, 0 };
         T_15_ThreeSum t_15_ThreeSum = new T_15_ThreeSum();
         List<List<Integer>> result = t_15_ThreeSum.threeSum(nums);
         for (List<Integer> list : result) {
