a6d5fcd6b774ca13accf3582e4e27f0f462387dd
wangxiang6 <wangxiang6@enmonster.com>
2025-03-29 15:31:33 +0800

    和我一开始的一个思路一致，但我没有贯彻下去

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_25_ReverseKGroup.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_25_ReverseKGroup.java
@@ -2,98 +2,54 @@ package com.wx.algorithm.leetcode.hop2025.hot100;
 
 import com.wx.algorithm.base.ListUtils;
 import com.wx.algorithm.base.model.ListNode;
-import com.wx.algorithm.leetcode.hop2025.hot100.T_25_ReverseKGroup.ListDTO;
 
 public class T_25_ReverseKGroup {
 
-    static class ListDTO {
-        ListNode start;
-        ListNode end;
-        ListNode next;
-        boolean finish;
-    }
-
-    public ListNode reverseKGroup_02(ListNode head, int k) {
-        ListNode start = head;
-        ListNode newHead = new ListNode();
-        ListNode pre = newHead;
-        ListDTO listDTO = new ListDTO();
-        int cnt = 0;
-        do {
-            listDTO = reverse(start, k);
-            pre.next = listDTO.start;
-            pre = listDTO.end;
-            if (cnt == 0) {
-                newHead.next = listDTO.start;
-            }
-            start = listDTO.next;
-        } while (!listDTO.finish);
-        return newHead.next;
-    }
-
-    private ListDTO reverse(ListNode start, int k) {
-        int cnt = 1;
-        ListNode a = start, b = start.next;
-        while (cnt != k && b != null) {
-            ListNode t = b.next;
-            b.next = a;
-            a = b;
-            b = t;
-            cnt++;
-        }
-        ListDTO listDTO = new ListDTO();
-        listDTO.start = a;
-        listDTO.end = start;
-        listDTO.next = b == null ? null : b.next;
-        listDTO.finish = b == null;
-        return listDTO;
-    }
-
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode pre = head, a = head, b = head.next;
-        int cnt = 1;
-        int n = 0;
-        ListNode newHead = new ListNode();
-        while (b != null) {
-            if (cnt == k) {
-                if (n == 0) {
-                    newHead = a;
-                    n++;
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
                 }
-                pre.next = b;
-                a = b;
-                b = b.next;
-                pre = a;
-                cnt = 1;
-            } else {
-                ListNode temp = b.next;
-                b.next = a;
-                a = b;
-                b = temp;
-                cnt++;
             }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
         }
 
-        if (cnt != 1) {
-            ListNode c = a.next;
-            a.next = null;
-            while (c != pre) {
-                ListNode temp = c.next;
-                c.next = a;
-                a = c;
-                c = temp;
-            }
-            c.next = a;
-        }
+        return hair.next;
+    }
 
-        return newHead;
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[] { tail, head };
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
         ListNode head = ListUtils.buildList(arr);
         T_25_ReverseKGroup reverseKGroup = new T_25_ReverseKGroup();
-        ListNode newHead = reverseKGroup.reverseKGroup_02(head, 2);
+        ListNode newHead = reverseKGroup.reverseKGroup(head, 2);
         ListUtils.printList(newHead);
     }
 }
