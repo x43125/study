473a64e7ee95db164259b3d9c86c339b5f9e9810
wangxiang6 <wangxiang6@enmonster.com>
2025-03-31 23:04:19 +0800

    annotation

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_148_SortList.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_148_SortList.java
@@ -17,14 +17,17 @@ public class T_148_SortList {
 
     private ListNode sortList(ListNode head, ListNode tail) {
         if (head == null) {
-            return null;
+            return head;
         }
 
+        // 如果head的下一个是tail，说明不用再拆分了
         if (head.next == tail) {
+            // 将每个节点拆散
             head.next = null;
             return head;
         }
 
+        // 快慢指针，找中点
         ListNode slow = head, fast = head;
         while (fast != tail) {
             slow = slow.next;
@@ -34,16 +37,19 @@ public class T_148_SortList {
             }
         }
 
+        // 递归核心
         ListNode mid = slow;
         ListNode list1 = sortList(head, mid);
+        // 因为上面处理的时候，没有算tail，所以此处可以用mid，而不用mid.next
         ListNode list2 = sortList(mid, tail);
-        ListNode sorted = merge(list1, list2);
-        return sorted;
+        ListNode merged = merge(list1, list2);
+        return merged;
     }
 
     private ListNode merge(ListNode head1, ListNode head2) {
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy, temp1 = head1, temp2 = head2;
+
         while (temp1 != null || temp2 != null) {
             if (temp1 == null) {
                 temp.next = temp2;
@@ -60,14 +66,14 @@ public class T_148_SortList {
                 temp.next = temp2;
                 temp2 = temp2.next;
             }
-
             temp = temp.next;
         }
+
         return dummy.next;
     }
 
     public static void main(String[] args) {
-        int[] arr = {};
+        int[] arr = { 4, 2, 1, 3, 0 };
         ListNode list = ListUtils.buildList(arr);
         T_148_SortList sortList = new T_148_SortList();
         ListNode newHead = sortList.sortList(list);
