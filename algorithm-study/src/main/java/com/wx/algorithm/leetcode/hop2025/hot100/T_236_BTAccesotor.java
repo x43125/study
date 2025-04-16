c9a6bd267a6aecb9c66861e8a6bfd902e538922b
wangxiang6 <wangxiang6@enmonster.com>
2025-04-16 22:37:33 +0800

    真绝了这答案，这谁想得到啊

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_236_BTAccesotor.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_236_BTAccesotor.java
@@ -1,39 +1,31 @@
 package com.wx.algorithm.leetcode.hop2025.hot100;
 
-import javax.swing.plaf.TreeUI;
-
 import com.wx.algorithm.base.TreeUtils;
 import com.wx.algorithm.base.model.TreeNode;
 
 public class T_236_BTAccesotor {
-    private TreeNode ans;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ans = null;
-        this.dfs(root, p, q);
-        return this.ans;
-    }
-
-    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return false;
+        // 遍历，如果当前节点为空，或为p或q，则返回
+        if (root == null || root == p || root == q) {
+            return root;
         }
 
-        boolean lson = dfs(root.left, p, q);
-        boolean rson = dfs(root.right, p, q);
+        // 遍历左子树，返回最先遇到的p/q，如果左子树返回空，说明遍历到底没找到p或q，则pq在右子树
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        // 遍历右子树，返回最先遇到的p/q，如果右子树返回空，说明遍历到底没找到p或q，则pq在左子树
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
 
-        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson)) ) {
-            ans = root;
-        }
-
-        return lson || rson || (root.val == p.val || root.val == q.val);
+        // 如果左右子树均不为空，则当前节点就是目标值
+        return l == null ? r : (r == null ? l : root);
     }
 
     public static void main(String[] args) {
         Integer[] nums = {3,5,1,6,2,0,8,null,null,7,4};
         TreeNode root = TreeUtils.buildTree(nums);
-        TreeNode p = new TreeNode(5);
-        TreeNode q = new TreeNode(1);
+        TreeUtils.printTree(root);
+        TreeNode p = root.left.right.left;
+        TreeNode q = root.left.right.right;
         T_236_BTAccesotor btAccesotor = new T_236_BTAccesotor();
         TreeNode lowestCommonAncestor = btAccesotor.lowestCommonAncestor(root, p, q);
         System.out.println(lowestCommonAncestor.val);
