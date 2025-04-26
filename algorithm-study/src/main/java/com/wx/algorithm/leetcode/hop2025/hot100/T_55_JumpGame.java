72ff967a684d3ba21b0d80d0ab78d3b36c7abcee
x43125 <wx43125@163.com>
2025-04-26 23:32:02 +0800

    跳台阶

--- algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_55_JumpGame.java
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_55_JumpGame.java
@@ -2,13 +2,29 @@ package com.wx.algorithm.leetcode.hop2025.hot100;
 
 public class T_55_JumpGame {
 
+    public boolean canJump_02(int[] nums) {
+        int n = nums.length;
+        int rightIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (rightIndex < i) {
+                return false;
+            }
+            rightIndex = Math.max(rightIndex, nums[i] + i);
+            if (rightIndex >= n - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     // 暴力递归，功能没问题，超时了
     public boolean canJump(int[] nums) {
         return canJump(nums, 0);
     }
 
     private boolean canJump(int[] nums, int i) {
-        if (i >= nums.length-1) {
+        if (i >= nums.length - 1) {
             return true;
         }
 
@@ -23,10 +39,11 @@ public class T_55_JumpGame {
     }
 
     public static void main(String[] args) {
-        // int[] nums = {2,3,1,1,4};
-        int[] nums = {3,2,1,0,4};
+        int[] nums = { 2, 3, 1, 1, 4 };
+        // int[] nums = { 3, 2, 1, 0, 4 };
+        // int[] nums = { 0 };
         T_55_JumpGame jumpGame = new T_55_JumpGame();
-        boolean canJump = jumpGame.canJump(nums);
+        boolean canJump = jumpGame.canJump_02(nums);
         System.out.println(canJump);
     }
 }
