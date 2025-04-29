926d89ecf8d0553c41e063f4aa6c0141791dcdfe
x43125 <wx43125@163.com>
2025-04-29 23:06:26 +0800

    feat: 完全平方数之和，还得练

---
+++ algorithm-study/src/main/java/com/wx/algorithm/leetcode/hop2025/hot100/T_279_NumSquares.java
@@ -0,0 +1,24 @@
+package com.wx.algorithm.leetcode.hop2025.hot100;
+
+public class T_279_NumSquares {
+    public int numSquares(int n) {
+        // f[n] = 1 + min(从1到根号(n-1))中f(n - j)
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, f[i-j*j]);
+            }
+            f[i] = 1 + min;
+        }
+
+        return f[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        T_279_NumSquares numSquares = new T_279_NumSquares();
+        int num = numSquares.numSquares(n);
+        System.out.println(num);
+    }
+}
