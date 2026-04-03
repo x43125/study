46453277cbe87922e406c61cf68ac097c86822d3
x43125 <wx43125@163.com>
2026-04-04 00:50:05 +0800

    order-service 引入 Apollo

--- spring-cloud-study/springcloud_parent/springcloud_09_sharding_order/src/main/java/com/zf/OrderApplication.java
+++ spring-cloud-study/springcloud_parent/springcloud_09_sharding_order/src/main/java/com/zf/OrderApplication.java
@@ -1,7 +1,6 @@
 package com.zf;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,7 +11,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableApolloConfig
-@MapperScan("com.zf.mapper")
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ spring-cloud-study/springcloud_parent/springcloud_09_sharding_order/src/main/java/com/zf/config/MybatisConfig.java
@@ -0,0 +1,45 @@
+package com.zf.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * MyBatis 配置类
+ * 手动配置 MyBatis 与 ShardingSphere 的集成
+ */
+@Configuration
+@MapperScan(basePackages = "com.zf.mapper", sqlSessionTemplateRef = "sqlSessionTemplate")
+public class MybatisConfig {
+
+    /**
+     * 创建 SqlSessionFactory
+     * 使用 ShardingSphere 创建的数据源
+     */
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("shardingSphereDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setTypeAliasesPackage("com.zf.entity");
+        bean.setMapperLocations(
+            new PathMatchingResourcePatternResolver()
+                .getResources("classpath:mapper/*.xml")
+        );
+        return bean.getObject();
+    }
+
+    /**
+     * 创建 SqlSessionTemplate
+     */
+    @Bean(name = "sqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
