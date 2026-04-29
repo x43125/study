31bf25bdcd2d34ffb75c77faacb3e323a6959ef7
x43125 <wx43125@163.com>
2026-04-29 09:24:31 +0800

    876

---
+++ algorithm-study/src/main/java/com/wx/algorithm/labuladong/2095.删除链表的中间节点.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=2095 lang=java
+ * @lcpr version=30403
+ *
+ * [2095] 删除链表的中间节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,3,4,7,1,2,6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1]\n
+// @lcpr case=end
+
+ */
+
---
+++ algorithm-study/src/main/java/com/wx/algorithm/labuladong/876.链表的中间结点.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=java
+ * @lcpr version=30403
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast  = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,6]\n
+// @lcpr case=end
+
+ */
+
