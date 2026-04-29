6ea5e7be7d93a2f97a10cd23f10d7fcdcb05e3fb
wangxiang6 <wangxiang6@enmonster.com>
2026-04-29 10:00:31 +0800

    25

--- algorithm-study/src/main/java/com/wx/algorithm/labuladong/25.K 个一组翻转链表.java
+++ algorithm-study/src/main/java/com/wx/algorithm/labuladong/25.K 个一组翻转链表.java
@@ -18,7 +18,54 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+        // K个一组，每到K个，执行翻转
+        // 不足K个的，不翻转：快慢指针，快指针先指向K个后，刚好也用来标志新头
+
+        ListNode headPreNode = new ListNode();
+        // 当前段的结尾节点
+        ListNode kthNode = head;
+
+        for (int i = 1; i < k; i++) {
+            if (kthNode != null) {
+                kthNode = kthNode.next;
+            } else {
+                return headPreNode.next;
+            }
+        }
+
+        // 前一段儿的尾巴
+        ListNode preSegTail = headPreNode;
+        ListNode node = head;
+        ListNode preNode = null;
+        ListNode nowSegTail;
+
+        while (kthNode != null) {
+            nowSegTail = node;
+            // 翻转当前K节点
+            ListNode nextSegHeadNode = kthNode.next;
+            while (node != nextSegHeadNode) {
+                ListNode next = node.next;
+                node.next = preNode;
+                preNode = node;
+                node = next;
+            }
+            preSegTail.next = kthNode;
+            preSegTail = nowSegTail;
+            kthNode = node;
+            nowSegTail.next = node;
+
+
+            // 将kthNode移动到下一个kth
+            for (int i = 1; i < k; i++) {
+                if (kthNode != null) {
+                    kthNode = kthNode.next;
+                } else {
+                    return headPreNode.next;
+                }
+            }
+        }
+
+        return headPreNode.next;
     }
 }
 // @lc code=end
